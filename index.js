cad5126a0d06ac5f9e99891c6008e728032c5c3b
curtismitchell <curtismitchell@gmail.com>
2014-09-19 17:13:50 +0000

    added some packages

---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+* Created with watchtower.
+* User: curtismitchell
+* Date: 2014-09-19
+* Time: 12:50 PM
+* To change this template use Tools | Templates.
+*/
+
+/*
+define(function() {
+return {};
+});
+*/
+
+var express = require('express');
+var app = express();
+var logger = require('morgan');
+
+// view engine settings
+app.set('views', './views');
+app.set('view engine', 'html');
+app.set('layout', 'layout');
+app.engine('html', require('hogan-express'));
+
+// logging
+app.use(logger('combined'));
+
+// routes
+app.get('/', function(req, res) {
+   res.send('you made it.'); 
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('listening...');
+});
